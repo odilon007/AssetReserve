1a6321d2bb4802b80fa0862d676027670519fcd9
Odilon Francelino de Pontes Neto <202514320015@ad.ifpb.edu.br>
2025-11-07 13:56:39 -0300

    Adicionando filtro

---
+++ AssetReserve/src/data.js
@@ -0,0 +1,19 @@
+const ativos = [
+    {
+        title: 'Sala de Reunião A',
+        image: './public/imgs/sala1.jpg',
+        tipo: 'sala',
+        id: 's1',
+        descricao: 'Capacidade: 8 pessoas'
+    },
+    {
+        title: 'Projetor Epson',
+        image: './public/imgs/projetor1.jpg',
+        tipo: 'equipamento',
+        id: 'e1',
+        descricao: 'Resolução: Full HD'
+    },
+];
+
+export default ativos;
+
--- AssetReserve/src/main.js
+++ AssetReserve/src/main.js
@@ -1,3 +1,30 @@
+import ativos from './data.js'
+
+// CARDS
+
+function ativocard(ativo) {
+  return `
+    
+      <div id="${ativo.id}" class="col-md-4 col-sm-6" data-category="${ativo.tipo}">
+        <div class="card shadow-sm ativo" data-category="sala" data-capacity="medium">
+          <div class="card-body">  
+            <img src="${ativo.image}" class="card-img-top" alt="${ativo.title}">
+            <h5 class="card-title">${ativo.title}</h5>
+            <p class="card-text text-muted">${ativo.descricao}</p>
+            <button class="btn open-modal btn-primary btn-reservar" data-id="1">Reservar</button>
+          </div> 
+        </div> 
+      </div>
+  `;
+}
+
+const allcards = ativos.map( (ativo) => ativocard(ativo));
+
+const main = document.querySelector('main');
+main.innerHTML = allcards.join('');
+
+//  MODAL
+
 const modal = document.getElementById("meuModal");
 const botoesAbrir = document.querySelectorAll(".open-modal");
 const btnFechar = document.getElementById("btnFechar")
@@ -17,9 +44,27 @@ botoesAbrir.forEach(btn => {
 });
 
 btnFechar.addEventListener("click", fecharModal);
-
+debugger
 modal.addEventListener("click", (e) => {
   if (e.target == modal) {
     fecharModal();
   }
 });
+
+//  FILTRAR CARDS PELO SELETOR
+
+document.getElementById('filter-category').addEventListener('change', function() {
+  const category = this.value;
+  const cards = document.querySelectorAll("#container .card")
+  cards.forEach(card => {
+    const cardCatergory = card.getAttribute('data-category');
+
+    // VERIFICA SE O CARD CORRESPONDE A SELECIONADA
+
+    if (category === '' || cardCatergory === category) {
+      card.style.display = "";
+    } else {
+      card.style.display = 'none';
+    }
+  })
+})
