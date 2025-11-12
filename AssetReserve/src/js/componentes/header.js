429bf9e03776602f3fb13d252f276a56a50af57d
Admin <admin@MacBook-Pro-de-Admin.local>
2025-11-12 11:33:39 -0300

    corrigir rotas para Githun Pages

--- AssetReserve/src/js/componentes/header.js
+++ AssetReserve/src/js/componentes/header.js
@@ -1,11 +1,11 @@
 export function carregarHeader() {
-    document.body.insertAdjacentHTML('afterbegin', `
+  document.body.insertAdjacentHTML('afterbegin', `
     <header>
     <nav>
       <a class="logo" href="./">AssetReserve</a>
       <ul class="nav-bar">
         <li><a href="./">Início</a></li>
-        <li><a href="./reservas.html">Reserva</a></li>
+        <li><a href="/Projeto-PW2/reservas.html">Reserva</a></li>
         <li><a href="./">Calendário</a></li>
         <li><a href="./">Fale conosco</a></li>
         <li><a href="./cadastro" class="btn btn-outline">Cadastrar</a></li>
