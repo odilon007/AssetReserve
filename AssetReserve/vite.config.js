ae284378421ab3fe251be6b024c2f006669ba8c0
Admin <admin@MacBook-Pro-de-Admin.local>
2025-11-12 11:41:47 -0300

    incluir múltiplas páginas no build

--- AssetReserve/vite.config.js
+++ AssetReserve/vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/Projeto-PW2/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reservas: resolve(__dirname, 'reservas.html'),
+      },
+    },
+  },
 })
