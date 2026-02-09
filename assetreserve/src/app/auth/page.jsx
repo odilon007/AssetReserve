90912a661759cbdad554e9dd86e04df56f2e5f08
Odilon <odilonpontes7@gmail.com>
2026-02-09 15:53:23 -0300

    Tornando auth como server component para dar deploy

--- assetreserve/src/app/auth/page.jsx
+++ assetreserve/src/app/auth/page.jsx
@@ -1,11 +1,9 @@
-'use client'
-
 import { Suspense } from 'react'
 import AuthContent from '@/components/auth/AuthContent'
 
 export default function Page() {
   return (
-    <Suspense>
+    <Suspense fallback={<div className="flex h-screen items-center justify-center">Carregando...</div>}>
       <AuthContent />
     </Suspense>
   )
